18c8793c70d92a45208160951f7214d8fe5b44be
= <matdombrock@gmail.com>
2022-10-10 18:54:59 -0700

    Advanced looping

--- cory.py
+++ cory.py
@@ -1,7 +1,7 @@
 import pyautogui
 import sys
 import commands
-
+import commandsUtil
 
 state = {
     "scriptLocation": "SCRIPT.txt",
@@ -32,8 +32,17 @@ def executeScript():
         split = line.split(' ')
         # Get the command
         cmd = split[0]
+        # Check for empty lines
+        if len(line) == 0:
+            nextLine()
+            continue # Move to the next line of the script
+        if line[0]==" ":
+            nextLine()
+            continue # Move to the next line of the script
+        # Check for comments
         if line[0]=="#":
             # This line is a comment
+            nextLine()
             continue # Move to the next line of the script
         # Make sure the command is in uppercase
         cmd = cmd.upper()
@@ -43,27 +52,8 @@ def executeScript():
             arg = split[1:]
             arg = ' '.join(arg)
         # Check if we have a loop command
-        if cmd == 'LOOP':
-            # Look for an existing loop
-            loopExists = False
-            for loop in state['loops']:
-                matchTag = loop['tag'] == arg
-                matchLine = loop['line'] != state['curLine']
-                if matchTag and matchLine: # found
-                    print('FOUND EXISTING LOOP: '+arg)
-                    state['curLine'] = loop['line']
-                    loopExists = True
-                    break
-            if loopExists: 
-                # We have set the line number to the desired loop point
-                continue # return to start of while loop
-            # Loop doest exist so append it
-            state['loops'].append({'tag':arg, 'line':state['curLine']})
-            print('CREATED NEW LOOP: '+arg)
-            # Move to next line
-            nextLine()
-            # Loop is a special command do not run it as a normal command
-            continue # return to start of while loop
+        if handleLoops(cmd, arg):
+            continue # Return to start of while loop
         # Check that we have a valid command
         foundCmd = False
         for availableCmd in commands.listing:
@@ -72,7 +62,7 @@ def executeScript():
         if foundCmd:
             res = commands.listing[cmd](arg, state)
             if(res != True):
-                print('ERROR: THERE WAS A PROBLEM WITH  YOUR LAST '+cmd+' COMMAND AT LINE '+str(state['curLine']))
+                print('ERROR: THERE WAS A PROBLEM WITH YOUR LAST '+cmd+' COMMAND AT LINE '+str(state['curLine']))
                 print('READ: '+line)
                 return # Quit
         else:
@@ -81,6 +71,68 @@ def executeScript():
         # Move to next line
         nextLine()
     print('SCRIPT COMPLETE!')
+
+def handleLoops(cmd, arg):
+    if cmd != 'LOOP':
+        # This was not a loop command
+        return False
+    argSplit = arg.split(' ')
+    loopTag = argSplit[0]
+    loopCount = 0
+    argHasCount = len(argSplit) > 1
+    if argHasCount:
+        loopCount = commandsUtil.safeInt(argSplit[1])
+    # Check if the loop exists already
+    foundLoop = {}
+    for loop in state['loops']:
+        matchTag = loop['tag'] == loopTag
+        if matchTag: # found
+            foundLoop = loop
+            break
+    if foundLoop != {} and argHasCount: 
+        print('FOUND EXISTING LOOP: '+loopTag)
+        # Set the line number to the desired loop point
+        if foundLoop['count'] == -1:
+            # This is the first time hitting this loop
+            # Set the loop count
+            foundLoop['count'] = loopCount-1
+            if foundLoop['count'] > 0:
+                # Set our current line to the loop point
+                state['curLine'] = foundLoop['line']
+                foundLoop['count'] = foundLoop['count'] -1
+                # Loop to loop point
+                print('START LOOPING: '+foundLoop['tag']+' ('+str(foundLoop['count'])+')')
+                return True  # return to start of while loop
+            else:
+                # Edge case
+                # We dont actually loop because it was set to one
+                print('LOOP END: '+foundLoop['tag'])
+                foundLoop['count'] = 0
+                nextLine()
+                return True  # return to start of while loop
+        elif foundLoop['count'] > 0:
+            # Loop back to the loop point
+            state['curLine'] = foundLoop['line']
+            # Remove one from loop count
+            foundLoop['count'] = foundLoop['count'] -1
+            print('LOOPING: '+foundLoop['tag']+' ('+str(foundLoop['count'])+')')
+            return True  # return to start of while loop
+        else:
+            # The loop has come to an end (no more count)
+            foundLoop['count'] = -1 # Reset loop
+            print('LOOP END: '+foundLoop['tag'])
+            nextLine()
+            return True  # return to start of while loop
+    elif foundLoop != {}:
+        print('FOUND EXISTING LOOP: '+loopTag+ ' WITH EMPTY COUNT')
+        state['curLine'] = foundLoop['line']
+        print('START LOOPING: '+foundLoop['tag']+' (INFINITE)')
+    if foundLoop == {}:
+        # Loop doest exist so append it
+        state['loops'].append({'tag':loopTag, 'line':state['curLine'], 'count':-1})
+        print('CREATED NEW LOOP: '+loopTag)
+    nextLine()
+    return True # return to start of while loop
 
 def nextLine():
     state['curLine'] = state['curLine']+1
